6a4d9ba4b724e2903fd81f233411e620b6eb2617
juanfurattini <juan.furattini@gmail.com>
2018-05-09 14:45:15 -0300

    Add audio attachment for songs

--- db/migrate/20180430221028_create_songs.rb
+++ db/migrate/20180430221028_create_songs.rb
@@ -4,6 +4,7 @@ class CreateSongs < ActiveRecord::Migration[5.2]
       t.string :name, null: false
       t.integer :duration
       t.string :source
+      t.attachment :audio
       t.references :musical_genre, foreign_key: true
       t.references :album, foreign_key: true
       t.references :artist, foreign_key: true
--- db/migrate/20180507204614_add_art_to_albums.rb
+++
@@ -1,9 +0,0 @@
-class AddArtToAlbums < ActiveRecord::Migration[5.2]
-  def up
-    #add_attachment :albums, :art
-  end
-
-  def down
-    #remove_attachment :albums, :art
-  end
-end
